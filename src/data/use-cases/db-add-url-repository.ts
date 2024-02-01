868a8e0f24b97d2830767ce261071f8c0c4f0a57
Anderson Naz <anderson_1004@hotmail.com>
2024-02-01 10:00:28 -0300

    feat: ensure return a ShortUrl if already exists

---
+++ src/data/use-cases/db-add-url-repository.ts
@@ -0,0 +1,26 @@
+import { AddShortenUrl } from "../../domain/use-cases/add-shorten-url";
+import { AddUrlRepository, LoadByLongerUrlRepository, ShortnerUrl } from "../protocols";
+
+export class DbAddUrlRepository implements AddShortenUrl {
+    private readonly addUrlRepository: AddUrlRepository
+    private readonly loadByLongerUrlRepository: LoadByLongerUrlRepository
+    private readonly shortner: ShortnerUrl
+
+    constructor({addUrlRepository, loadUrlRepository, shortner}: AddShortenUrl.Dependencies){
+        this.addUrlRepository = addUrlRepository
+        this.loadByLongerUrlRepository = loadUrlRepository
+        this.shortner = shortner
+    }
+    
+    async add(longUrl: AddShortenUrl.Params): Promise<AddShortenUrl.Result> {
+        const shortUrl = await this.loadByLongerUrlRepository.load(longUrl)
+        if(shortUrl){
+            return {
+                id: shortUrl.id,
+                shortUrl: shortUrl.shortUrl,
+                longUrl: longUrl
+            }
+        }
+    }
+    
+}
