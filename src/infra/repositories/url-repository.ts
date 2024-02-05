ad17bf54cef1e34afcf01b2ebfab0b1165ce4d22
Anderson Naz <anderson_1004@hotmail.com>
2024-02-05 20:31:37 -0300

    feat: add load method in UrlRepository

--- src/infra/repositories/url-repository.ts
+++ src/infra/repositories/url-repository.ts
@@ -1,10 +1,16 @@
-import { AddUrlRepository } from "../../data/protocols";
+import { AddUrlRepository, LoadByLongerUrlRepository } from "../../data/protocols";
 import { parseMongoDocumentToUrl, urlMongo } from "./mongodb/mongo-helper";
 
-export class UrlRepository implements AddUrlRepository {
+export class UrlRepository implements AddUrlRepository, LoadByLongerUrlRepository {
     async add(params: AddUrlRepository.Params): Promise<AddUrlRepository.Result> {
         const url = await urlMongo.create(params)
         const result = parseMongoDocumentToUrl(url)
         return result
+    }
+
+    async load(longerUrl: LoadByLongerUrlRepository.Params): Promise<LoadByLongerUrlRepository.Result> {
+        const url = await urlMongo.findOne({ longUrl: longerUrl})
+        const longUrl = parseMongoDocumentToUrl(url)
+        return longUrl
     }
 }
