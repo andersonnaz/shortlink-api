1aca1c74e262d52ef944be82027e37a36d9b52a5
Anderson Naz <anderson_1004@hotmail.com>
2024-02-05 19:26:36 -0300

    test: ensure return 400 (serverError) if shortenUrl throws

---
+++ tests/application/controllers/shorten-url.spec.ts
@@ -0,0 +1,50 @@
+import { ShortenUrl } from "../../../src/application/controllers/shorten-url"
+import { HttpRequest, HttpResponse, serverError } from "../../../src/application/helpers"
+import { AddShortenUrl } from "../../../src/domain/use-cases"
+
+const makeAddShortenUrl = (): AddShortenUrl => {
+    class AddShortenUrlStub implements AddShortenUrl {
+        add(longUrl: AddShortenUrl.Params): Promise<AddShortenUrl.Result> {
+            return new Promise(resolve => resolve({
+                id: 'any_id',
+                shortUrl: 'any_url',
+                longUrl: 'any_url'
+            }))
+        }
+    }
+    return new AddShortenUrlStub()
+}
+
+interface SutTypes {
+    sut: ShortenUrl
+    shortenUrlStub: AddShortenUrl
+}
+
+const makeSut = (): SutTypes => {
+    const addShortenUrlStub = makeAddShortenUrl()
+    const dependencies: ShortenUrl.Dependencies = {
+        shortenUrl: addShortenUrlStub
+    }
+    const sut = new ShortenUrl(dependencies)
+    return {
+        sut,
+        shortenUrlStub: addShortenUrlStub
+    }
+}
+
+describe('ShortenUrl', () => {
+    const fakeHttpRequest: HttpRequest = {
+        body: {
+            longUrl: 'any_url'
+        }
+    }
+
+    it('Should return 500 (ServerError) if ShortenUrl throws', async () => {
+        const { sut, shortenUrlStub} = makeSut()
+        const fakeError = new Error()
+        fakeError.stack = 'any_stack'
+        jest.spyOn(shortenUrlStub, 'add').mockRejectedValueOnce(fakeError)
+        const httpResponse = await sut.handle(fakeHttpRequest)
+        expect(httpResponse).toEqual(serverError(fakeError))
+    })
+})
