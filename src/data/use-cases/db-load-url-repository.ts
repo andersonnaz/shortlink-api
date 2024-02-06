ca91a1f4f7180edc01d56c99489de323b9dd280b
Anderson Naz <anderson_1004@hotmail.com>
2024-02-06 11:14:32 -0300

    feat: create DbLoadUrlRepository use case

---
+++ src/data/use-cases/db-load-url-repository.ts
@@ -0,0 +1,19 @@
+import { LoadLongerUrl } from "../../domain/use-cases";
+import { UrlRepository } from "../../infra/repositories/url-repository";
+import { LoadUrlRepository } from "../protocols";
+
+export class DbLoadUrlRepository implements LoadLongerUrl {
+    private readonly urlRepository: UrlRepository
+
+    constructor({urlRepository}: LoadLongerUrl.Dependencies){
+        this.urlRepository = urlRepository
+    }
+
+    async load(shortUrl: LoadUrlRepository.Params): Promise<LoadUrlRepository.Result> {
+        const longUrl = await this.urlRepository.loadLongUrl(shortUrl)
+        if(!longUrl){
+            return null
+        }
+        return longUrl
+    }
+}
