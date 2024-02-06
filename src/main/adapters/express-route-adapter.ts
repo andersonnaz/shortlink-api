94b260154962e5d2af93f9b9bf64ee286819f871
Anderson Naz <anderson_1004@hotmail.com>
2024-02-06 09:04:54 -0300

    feat: create express route adapter

---
+++ src/main/adapters/express-route-adapter.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+import { Controller } from "../../application/protocols/controller";
+import { HttpRequest, HttpResponse } from "../../application/helpers";
+
+export const adaptRoute = (controller : Controller) => {
+    return async (request: Request, response: Response) => {
+        const httpRequest: HttpRequest = {
+            body: request.body
+        }
+        const httpResponse: HttpResponse = await controller.handle(httpRequest)
+        response.status(httpResponse.statusCode).json(httpResponse.body)
+    }
+}
