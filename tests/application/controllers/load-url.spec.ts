d379eb0a3457acd1b1c3dfffe4608463ed8ab3d1
Anderson Naz <anderson_1004@hotmail.com>
2024-02-06 11:58:46 -0300

    test: ensure return 500 (serverError) if LoadUrl throws

---
+++ tests/application/controllers/load-url.spec.ts
@@ -0,0 +1,47 @@
+import { LoadUrl } from "../../../src/application/controllers"
+import { HttpRequest, serverError } from "../../../src/application/helpers"
+import { LoadLongerUrl } from "../../../src/domain/use-cases"
+
+const makeLoadLongerUrl = (): LoadLongerUrl => {
+    class LoadLongerUrlStub implements LoadLongerUrl {
+        load(shortUrl: LoadLongerUrl.Params): Promise<LoadLongerUrl.Result> {
+            return new Promise(resolve => resolve({
+                longUrl: 'any_url'
+            }))
+        }
+    }
+    return new LoadLongerUrlStub()
+}
+
+interface SutTypes {
+    sut: LoadUrl,
+    loadUrlStub: LoadLongerUrl
+}
+
+const makeSut = (): SutTypes => {
+    const loadUrlStub = makeLoadLongerUrl()
+    const sut = new LoadUrl({
+        loadUrl: loadUrlStub
+    })
+    return {
+        sut,
+        loadUrlStub
+    }
+}
+
+describe('LoadUrl', () => {
+    const fakeHttpRequest: HttpRequest = {
+        body: {
+            shortUrl: 'any_url'
+        }
+    }
+    
+    it('Should return 500 (ServerError) if LoadUrl throws', async () => {
+        const { sut, loadUrlStub } = makeSut()
+        const fakeError = new Error()
+        fakeError.stack = 'any_stack'
+        jest.spyOn(loadUrlStub, 'load').mockRejectedValueOnce(fakeError)
+        const httpResponse = await sut.handle(fakeHttpRequest)
+        expect(httpResponse).toEqual(serverError(fakeError))
+    })
+})
