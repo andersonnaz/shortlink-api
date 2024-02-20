a06baccbe29bc1b12bec3bec1fa1973600fbb655
Anderson Naz <anderson_1004@hotmail.com>
2024-02-20 10:46:07 -0300

    fix: resolve middleware cors error

--- src/main/middlewares/cors.ts
+++ src/main/middlewares/cors.ts
@@ -1,8 +1,8 @@
-import { NextFunction, Request, Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 
 export const cors = (request: Request, response: Response, next: NextFunction) => {
     response.set('access-control-allow-origin', '*')
     response.set('access-control-allow-methods', '*')
-    response.set('access-controll-allow-headers', '*')
+    response.set('access-control-allow-headers', '*')
     next()
 }
