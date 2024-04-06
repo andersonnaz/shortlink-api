1cb2ac1d447a09b9aa822853f08718e5074bf202
andersonnaz <anderson_1004@hotmail.com>
2024-04-06 20:34:36 -0300

    feat: create AddUser Use Case protocol

---
+++ src/domain/use-cases/add-user.ts
@@ -0,0 +1,26 @@
+import { HashGenerator } from "../../data/protocols/cryptography"
+import { AddUserRepository } from "../../data/protocols/db"
+import { User } from "../models/user"
+
+export interface AddUser {
+    add(params : AddUser.Params): Promise<AddUser.Result>
+}
+
+export namespace AddUser {
+    export type Params = {
+        name: string
+        email: string
+        password: string
+    }
+
+    export type Result = {
+        isSuccess: boolean
+        error?: Error
+        data?: User
+    }
+
+    export type Dependencies = {
+        userRepository: AddUserRepository
+        hashServise: HashGenerator
+    }
+}
