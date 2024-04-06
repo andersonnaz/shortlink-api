add5649391dc700800b26a0852a055eeaf5bdea7
andersonnaz <anderson_1004@hotmail.com>
2024-04-06 20:32:05 -0300

    feat: create AddUserRepository protocol

---
+++ src/data/protocols/db/add-user-repository.ts
@@ -0,0 +1,19 @@
+import { User } from "../../../domain/models/user"
+
+export interface AddUserRepository {
+    add(params: AddUserRepository.Params): Promise<AddUserRepository.Result>
+}
+
+export namespace AddUserRepository {
+    export type Params = {
+        name: string
+        email: string
+        password: string
+    }
+
+    export type Result = {
+        isSuccess: boolean
+        error?: Error
+        data?: User
+    }
+}
