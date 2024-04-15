222a7b60b6cdcd29de2579cb428fc9ce4a83a0ae
Anderson Naz <anderson_1004@hotmail.com>
2024-04-15 10:53:51 -0300

    feat: create LoadUserByEmailrepository protocol

---
+++ src/data/protocols/db/user/load-user-by-email-repository.ts
@@ -0,0 +1,16 @@
+export interface LoadUserByEmailRepository {
+    load(params: LoadUserByEmailRepository.Params): Promise<LoadUserByEmailRepository.Result>
+}
+
+export namespace LoadUserByEmailRepository {
+    export type Params = {
+        email: string
+    }
+
+    export type Result = {
+        id: string
+        name: string
+        email: string
+        password: string
+    } | undefined
+}
