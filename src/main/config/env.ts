f3f14c62af72fc138d6d142483c1b6d078727830
Anderson Naz <anderson_1004@hotmail.com>
2024-04-15 11:26:41 -0300

    feat: add SALT in dotenv and config file

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -3,6 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 export default {
     PORT: process.env.PORT || 3000,
-    MONGO_URL: process.env.MONGO_URL || ""
-
+    MONGO_URL: process.env.MONGO_URL || "",
+    SALT: process.env.SALT || 12
 }
