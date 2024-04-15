245c3f8cf1df4ca9caaff03a712d8279066b9bd7
Anderson Naz <anderson_1004@hotmail.com>
2024-04-15 11:28:32 -0300

    feat: create signup route

--- src/main/config/routes.ts
+++ src/main/config/routes.ts
@@ -1,10 +1,13 @@
 import { Express, Router } from "express"
 import shortenUrlRoute from "../routes/shorten-url-route"
 import loadUrlRoute from "../routes/load-url-route"
+import signUpRoute from "../routes/add-user-route"
 
 export default (app: Express): void => {
     const router = Router()
     app.use('/api', router)
     shortenUrlRoute(router)
     loadUrlRoute(router)
+    signUpRoute(router)
+
 }
