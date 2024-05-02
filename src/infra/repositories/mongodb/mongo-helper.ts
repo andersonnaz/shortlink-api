39cb2605bf9b219eb7c7192b427babe9fa68adf7
Anderson Naz <anderson_1004@hotmail.com>
2024-05-02 11:05:01 -0300

    feat: create mongo helper to tokenManager

--- src/infra/repositories/mongodb/mongo-helper.ts
+++ src/infra/repositories/mongodb/mongo-helper.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, model } from "mongoose";
 import { Url } from "../../../domain/models/url";
-import { urlSchema, userSchema } from "./mongo-schemas";
+import { tokenManagerSchema, urlSchema, userSchema } from "./mongo-schemas";
 import { User } from "../../../domain/models/user";
 
 export interface IUrl extends Document {
@@ -15,11 +15,26 @@ export interface IUser extends Document {
     email: string
     password: string
     verifiedEmail: boolean
+}
 
+export interface ITokenManager extends Document {
+    _id: any
+    token: string
+    tokenCreatedAt: Date
+    userId: string
+    recoveryPasswordCode: number
+}
+
+interface TokenManager {
+    token: string
+    tokenCreatedAt: Date
+    userId: string
+    recoveryPasswordCode: number
 }
 
 const urlMongo = model<Url>("Url", urlSchema())
 const userMongo = model<User>("User", userSchema())
+const tokenManager = model<TokenManager>("TokenManager", tokenManagerSchema())
 
 const parseMongoDocumentToUrl = (url: IUrl) => {
     if(!url){
@@ -45,6 +60,19 @@ const parseMongoDocumentToUser = (user: IUser) => {
     }
 }
 
+const parseMongoDocumentToTokenManager = (params: ITokenManager) => {
+    if(!params){
+        return undefined
+    }
+    return {
+        id: params._id,
+        token: params.token,
+        tokenCreatedAt: params.tokenCreatedAt,
+        userId: params.userId,
+        RecoveryPasswordCode: params.recoveryPasswordCode
+    }
+}
+
 const connectMongo = async (uri: string) => {
     await mongoose.connect(uri)
 }
@@ -52,7 +80,9 @@ const connectMongo = async (uri: string) => {
 export {
     urlMongo,
     userMongo,
+    tokenManager,
     parseMongoDocumentToUrl,
     parseMongoDocumentToUser,
+    parseMongoDocumentToTokenManager,
     connectMongo
 }
