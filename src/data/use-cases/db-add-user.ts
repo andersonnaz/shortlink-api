0a238784fa2ce1cae9eee944bdab4eabf583e601
andersonnaz <anderson_1004@hotmail.com>
2024-05-05 00:38:12 -0300

    feat: add emailService in AddUser implementation

--- src/data/use-cases/db-add-user.ts
+++ src/data/use-cases/db-add-user.ts
@@ -1,14 +1,17 @@
 import { AddUser } from "../../domain/use-cases/add-user";
+import { EmailService } from "../protocols/communication/email-service";
 import { HashGenerator } from "../protocols/cryptography";
 import { AddUserRepository } from "../protocols/db/user";
 
 export class DbAddUser implements AddUser {
     private readonly userRepository: AddUserRepository
     private readonly hashService: HashGenerator
+    private readonly emailService: EmailService
 
-    constructor({ userRepository, hashService }: AddUser.Dependencies){
+    constructor({ userRepository, hashService, emailService }: AddUser.Dependencies){
         this.userRepository = userRepository
         this.hashService = hashService
+        this.emailService = emailService
     }
 
     async add(params: AddUser.Params): Promise<AddUser.Result> {
@@ -18,6 +21,23 @@ export class DbAddUser implements AddUser {
             email: params.email,
             password: hashedPassword
         })
+        const emailConfirmationSent = this.emailConfirmation(user.name, user.email)
+        if(!emailConfirmationSent){
+            return undefined
+        }
         return user
     }
+
+    private async emailConfirmation (name: string, email: string): Promise<boolean> {
+        const result = await this.emailService.send({
+            from: process.env.MAIL_USER,
+            subject: 'Email confirmation',
+            to: email,
+            text: `
+                Olá ${name}
+                click no link abaixo para confirmar seu email
+            `
+        })
+        return result
+    } 
 }
