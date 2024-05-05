1f22fb1efab439103e1eaa67e9170f85b1e1c15c
andersonnaz <anderson_1004@hotmail.com>
2024-05-05 00:34:28 -0300

    feat: add email service credentials in dotenv

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -4,5 +4,8 @@ dotenv.config()
 export default {
     PORT: process.env.PORT || 3000,
     MONGO_URL: process.env.MONGO_URL || "",
-    SALT: process.env.SALT || 12
+    SALT: process.env.SALT || 12,
+    MAIL_SERVICE: process.env.MAIL_SERVICE,
+    MAIL_USER: process.env.MAIL_USER,
+    MAIL_PASS: process.env.MAIL_PASS
 }
